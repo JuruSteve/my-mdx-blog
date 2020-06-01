cd558df945fbe637500873936ff769a8fb364aa7
jurusteve <jstevemu17@gmail.com>
2020-06-01 13:54:11 -0400

    added a featured image component

---
+++ src/components/FeatureImage.js
@@ -0,0 +1,31 @@
+import React from "react"
+import Image from "gatsby-image"
+import { useStaticQuery, graphql } from "gatsby"
+import { FeatureImageWrapper } from "../elements"
+
+export const FeatureImage = ({ fixed }) => {
+  const data = useStaticQuery(graphql`
+    query {
+      imageSharp(fixed: { originalName: { eq: "gatsby-astronaut.png" } }) {
+        fixed {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+  `)
+
+  return (
+    <FeatureImageWrapper>
+      <Image
+        fixed={fixed ? fixed : data.imageSharp.fixed}
+        style={{
+          position: "absolute",
+          left: 0,
+          top: 0,
+          width: "100%",
+          height: "100%",
+        }}
+      />
+    </FeatureImageWrapper>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 import React from "react"
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
-
+import { FeatureImage } from "../components"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
+    <FeatureImage />
   </Layout>
 )
 
